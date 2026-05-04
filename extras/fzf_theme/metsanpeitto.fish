07d061427cffd44f37a7485164c714c6ad3899c7
fallwith <fallwith@gmail.com>
2026-05-03 22:56:44 -0700

    added fzf theme to extras

---
+++ extras/fzf_theme/metsanpeitto.fish
@@ -0,0 +1,5 @@
+# fzf metsanpeitto theme
+set -gx FZF_DEFAULT_OPTS "\
+--color=bg+:#282a2e,bg:#1d2230,spinner:#8abeb7,hl:#81a2be \
+--color=fg:#f5ecd7,header:#81a2be,info:#f0c674,pointer:#8abeb7 \
+--color=marker:#8abeb7,fg+:#e0e0e0,prompt:#f0c674,hl+:#81a2be"
